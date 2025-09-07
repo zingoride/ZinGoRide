a6cd7706ea6e8ae011f61091c52d48782e400f56
ZinGo Ride <zingoride@gmail.com>
2025-09-07 13:41:40 +0000

    UbdZcK9ztxdmMbo_1G8HVgLc5yE

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
    devIndicators: {
